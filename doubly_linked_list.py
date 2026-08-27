7804da2deef81a5e7131eee15893b45bda03c55b
Ayush090504 <ayushsnh2004@gmail.com>
2026-08-27 11:19:44 +0530

    doubly_linked_list implementation still not full

---
+++ doubly_linked_list.py
@@ -0,0 +1,79 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+        self.prev=None
+
+def insert_at_beginning(head):
+    val=int(input('Enter the value to be inserted: '))
+    newNode=Node(val)
+    if head is None:
+        head=newNode
+        return head
+    newNode.next=head
+    head.prev=newNode
+    head=newNode
+    return head
+
+def insertion_at_the_end(head):
+    val=int(input('Enter the new value to be inserted: '))
+    newNode=Node(val)
+    if head is None:
+        head=newNode
+        return head
+    temp=head
+    while temp.next is not None:
+        temp=temp.next
+    temp.next=newNode
+    newNode.prev=temp
+    return head
+
+def delete_at_the_beginning(head):
+    if head is None:
+        print('The linked list is empty!!')
+        return head
+    elif head.next is None:
+        head=None
+        return head
+    head=head.next
+    head.prev=None
+    return head
+
+def delete_at_the_end(head):
+    if head is None:
+        print('Linked List is empty!!')
+        return head
+    elif head.next is None:
+        head=None
+        return head
+    temp=head
+    while temp.next is not None:
+        temp=temp.next
+    temp=temp.prev
+    temp.next=None
+    return head
+
+def forward_traversal(head):
+    if head is None:
+        print('The linked list is empty!!')
+        return head
+    temp=head
+    while temp is not None:
+        print(temp.data, end=' ')
+        temp=temp.next
+    print()
+    return head
+
+head=None
+while True:
+    op=int(input('Enter the operation: '))
+    if op==1:
+        head=insert_at_beginning(head)
+    elif op == 2:
+        head=insertion_at_the_end(head)
+    elif op == 3:
+        head=delete_at_the_beginning(head)
+    elif op == 4:
+        head=delete_at_the_end(head)
+    elif op == 5:
+        head=forward_traversal(head)
