573f666aa7a98a2633fcd8da726cc090a1a1dbed
Ayush090504 <ayushsnh2004@gmail.com>
2026-08-27 13:51:52 +0530

    partial completed circular_linked_list

--- circular_linked_list.py
+++ circular_linked_list.py
@@ -3,7 +3,55 @@ class Node:
         self.data=data
         self.next=None
 
+def insert_at_the_beginning(head):
+    val=int(input('Enter the new value to be inserted: '))
+    newNode=Node(val)
+    if head is None:
+        head=newNode
+        head.next=head
+        return head
+    newNode.next=head
+    temp=head
+    while temp.next != head:
+        temp=temp.next
+    temp.next=newNode
+    head=newNode
+    return head
+
+def insert_at_the_end(head):
+    val=int(input('Enter the value to be inserted: '))
+    newNode=Node(val)
+    if head is None:
+        head=newNode
+        head.next=head
+        return head
+    temp=head
+    while temp.next != head:
+        temp=temp.next
+    temp.next=newNode
+    newNode.next=head
+    return head
+
+def traversal(head):
+    if head is None:
+        print('The linked list is empty!!')
+        return head
+    temp=head
+    while True:
+        print(temp.data, end=' ')
+        temp=temp.next
+
+        if temp == head:
+            break
+    print()
+    return head
+    
 head=None
 while True:
     op=int(input('Enter the operation to be performed: '))
-    
+    if op == 1:
+        head=insert_at_the_beginning(head)
+    elif op == 2:
+        head=insert_at_the_end(head)
+    elif op == 3:
+        head=traversal(head)
